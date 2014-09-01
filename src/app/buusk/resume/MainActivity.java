25e721a2da0472a4b43495d192e122572e5a4fea
gamekk <gamebuu001@gmail.com>
2014-09-01 17:14:33 +0700

    Resume

---
+++ src/app/buusk/resume/MainActivity.java
@@ -0,0 +1,32 @@
+package app.buusk.resume;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+
+public class MainActivity extends Activity {
+	private Button btn1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn1 = (Button) findViewById(R.id.button2);
+        btn1.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent i = new Intent(getApplicationContext(),ImageActivity.class);
+				startActivity(i);
+				
+				
+			}
+		});
+        
+    }
+}
